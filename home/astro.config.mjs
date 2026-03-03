c6c8edc54001443ec99b3cf3c98f792b3552b8ec
Thomas Ball <thomas.j.ball@outlook.com>
2026-03-03 14:36:06 +0000

    add react

--- home/astro.config.mjs
+++ home/astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlight from '@astrojs/starlight';
+import react from '@astrojs/react';
+
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
+		    react(), // Enable React support
 		starlight({
 			title: 'My Docs',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
