b2b8d02d09efc72b768686bf3c0290db1bed653e
Thomas Ball <thomas.j.ball@outlook.com>
2026-03-04 11:28:45 +0000

    remove duplicate

--- home/astro.config.mjs
+++ home/astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-import starlight from '@astrojs/starlight';
 import react from '@astrojs/react';
 
 
